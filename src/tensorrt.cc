f3b5394e3d00edb7f8674a92cadc206296e8dc7a
Tanmay Verma <tanmay2592@gmail.com>
2021-04-07 09:33:25 -0700

    Build and simple model load

---
+++ src/tensorrt.cc
@@ -0,0 +1,509 @@
+// Copyright (c) 2021, NVIDIA CORPORATION. All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//  * Redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+//  * Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in the
+//    documentation and/or other materials provided with the distribution.
+//  * Neither the name of NVIDIA CORPORATION nor the names of its
+//    contributors may be used to endorse or promote products derived
+//    from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ``AS IS'' AND ANY
+// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+// PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+// OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#include "loader.h"
+#include "tensorrt_utils.h"
+#include "triton/backend/backend_common.h"
+#include "triton/backend/backend_input_collector.h"
+#include "triton/backend/backend_model.h"
+#include "triton/backend/backend_model_instance.h"
+#include "triton/backend/backend_output_responder.h"
+
+#include <atomic>
+#include <chrono>
+#include <map>
+#include <memory>
+#include <set>
+#include <thread>
+#include <unordered_map>
+
+#ifdef TRITON_ENABLE_GPU
+#include <cuda_runtime_api.h>
+#endif  // TRITON_ENABLE_GPU
+
+//
+// TensorRT Backend that implements the TRITONBACKEND API.
+//
+namespace triton { namespace backend { namespace tensorrt {
+
+//
+// ModelState
+//
+// State associated with a model that is using this backend. An object
+// of this class is created and associated with each
+// TRITONBACKEND_Model.
+//
+class ModelState : public BackendModel {
+ public:
+  static TRITONSERVER_Error* Create(
+      TRITONBACKEND_Model* triton_model, ModelState** state);
+  virtual ~ModelState() = default;
+
+  TRITONSERVER_Error* CreateEngine(
+      int gpu_device, const std::string& model_path);
+
+ private:
+  ModelState(TRITONBACKEND_Model* triton_model);
+
+  // Auto-complete the model configuration
+  TRITONSERVER_Error* AutoCompleteConfig();
+
+  // Parses the parameters in config
+  TRITONSERVER_Error* ParseParameters();
+
+  // Validate that model configuration is supported by this backend.
+  TRITONSERVER_Error* ValidateModelConfig();
+
+  // CUDA engine shared across all model instances on the same device.
+  std::map<int, std::pair<nvinfer1::IRuntime*, nvinfer1::ICudaEngine*>>
+      device_engines_;
+
+  // Next context to be used for the runner.
+  std::vector<size_t> next_context_;
+};
+
+TRITONSERVER_Error*
+ModelState::Create(TRITONBACKEND_Model* triton_model, ModelState** state)
+{
+  try {
+    *state = new ModelState(triton_model);
+  }
+  catch (const BackendModelException& ex) {
+    RETURN_ERROR_IF_TRUE(
+        ex.err_ == nullptr, TRITONSERVER_ERROR_INTERNAL,
+        std::string("unexpected nullptr in BackendModelException"));
+    RETURN_IF_ERROR(ex.err_);
+  }
+
+  // Auto-complete the configuration if requested...
+  bool auto_complete_config = false;
+  RETURN_IF_ERROR(TRITONBACKEND_ModelAutoCompleteConfig(
+      triton_model, &auto_complete_config));
+  if (auto_complete_config) {
+    RETURN_IF_ERROR((*state)->AutoCompleteConfig());
+
+    triton::common::TritonJson::WriteBuffer json_buffer;
+    (*state)->ModelConfig().Write(&json_buffer);
+
+    TRITONSERVER_Message* message;
+    RETURN_IF_ERROR(TRITONSERVER_MessageNewFromSerializedJson(
+        &message, json_buffer.Base(), json_buffer.Size()));
+    RETURN_IF_ERROR(TRITONBACKEND_ModelSetConfig(
+        triton_model, 1 /* config_version */, message));
+  }
+
+  RETURN_IF_ERROR((*state)->ValidateModelConfig());
+  RETURN_IF_ERROR((*state)->ParseParameters());
+
+  return nullptr;  // success
+}
+
+ModelState::ModelState(TRITONBACKEND_Model* triton_model)
+    : BackendModel(triton_model)
+{
+  // Obtain backend configuration
+  TRITONBACKEND_Backend* backend;
+  THROW_IF_BACKEND_MODEL_ERROR(
+      TRITONBACKEND_ModelBackend(triton_model, &backend));
+  // void* vstate;
+  // THROW_IF_BACKEND_MODEL_ERROR(TRITONBACKEND_BackendState(backend, &vstate));
+  // backend_config_ = reinterpret_cast<BackendConfiguration*>(vstate);
+}
+
+TRITONSERVER_Error*
+ModelState::CreateEngine(int gpu_device, const std::string& model_path)
+{
+  // Create shared engine for the device if haven't tried so.
+  auto eit = device_engines_.find(gpu_device);
+  if (eit == device_engines_.end()) {
+    eit = device_engines_.emplace(gpu_device, std::make_pair(nullptr, nullptr))
+              .first;
+
+    // Create a CUDA engine shared by all contexts
+    auto cuerr = cudaSetDevice(gpu_device);
+    if (cuerr != cudaSuccess) {
+      return TRITONSERVER_ErrorNew(
+          TRITONSERVER_ERROR_INTERNAL,
+          (std::string("unable to set device for ") + Name() + ": " +
+           cudaGetErrorString(cuerr))
+              .c_str());
+    }
+
+    RETURN_IF_ERROR(
+        LoadPlan(model_path, &eit->second.first, &eit->second.second));
+    // Validate whether the engine can be shared
+    bool is_dynamic = false;
+    for (int idx = 0; idx < eit->second.second->getNbBindings(); idx++) {
+      auto dims = eit->second.second->getBindingDimensions(idx);
+      // Detect whether dynamic or not
+      if (ContainsWildcard(dims)) {
+        is_dynamic = true;
+        break;
+      }
+    }
+    // Model with dynamic shapes can't share engine, set to engine to
+    // 'nullptr' as hint, but keeping runtime as it can be used repeatedly
+    if (is_dynamic) {
+      if (eit->second.second != nullptr) {
+        eit->second.second->destroy();
+        eit->second.second = nullptr;
+      }
+    }
+  }
+
+  return nullptr;
+}
+
+
+TRITONSERVER_Error*
+ModelState::AutoCompleteConfig()
+{
+  return nullptr;  // success
+}
+
+TRITONSERVER_Error*
+ModelState::ValidateModelConfig()
+{
+  return nullptr;  // success
+}
+
+TRITONSERVER_Error*
+ModelState::ParseParameters()
+{
+  return nullptr;  // success
+}
+
+//
+// ModelInstanceState
+//
+// State associated with a model instance. An object of this class is
+// created and associated with each TRITONBACKEND_ModelInstance.
+//
+class ModelInstanceState : public BackendModelInstance {
+ public:
+  // GPU device number that indicates that no gpu is available for a
+  // context (which is an invalid state since TensorRT requires a
+  // GPU).
+  static constexpr int NO_GPU_DEVICE = -1;
+
+  // GPU device number that indicates model will be loaded on GPUs
+  // as specified in model graph
+  static constexpr int MODEL_DEVICE = -2;
+
+  static TRITONSERVER_Error* Create(
+      ModelState* model_state,
+      TRITONBACKEND_ModelInstance* triton_model_instance,
+      ModelInstanceState** state);
+  virtual ~ModelInstanceState() = default;
+
+  // Get the state of the model that corresponds to this instance.
+  ModelState* StateForModel() const { return model_state_; }
+
+  void ProcessRequests(
+      TRITONBACKEND_Request** requests, const uint32_t request_count);
+
+ private:
+  ModelInstanceState(
+      ModelState* model_state,
+      TRITONBACKEND_ModelInstance* triton_model_instance);
+
+  ModelState* model_state_;
+};
+
+TRITONSERVER_Error*
+ModelInstanceState::Create(
+    ModelState* model_state, TRITONBACKEND_ModelInstance* triton_model_instance,
+    ModelInstanceState** state)
+{
+  try {
+    *state = new ModelInstanceState(model_state, triton_model_instance);
+  }
+  catch (const BackendModelInstanceException& ex) {
+    RETURN_ERROR_IF_TRUE(
+        ex.err_ == nullptr, TRITONSERVER_ERROR_INTERNAL,
+        std::string("unexpected nullptr in BackendModelInstanceException"));
+    RETURN_IF_ERROR(ex.err_);
+  }
+
+  // If the model configuration doesn't have an explicit model file
+  // specified then use the default name.
+  std::string cc_model_filename = (*state)->ArtifactFilename();
+  if (cc_model_filename.empty()) {
+    cc_model_filename = "model.plan";
+  }
+
+  auto model_path =
+      JoinPath({model_state->RepositoryPath(),
+                std::to_string(model_state->Version()), cc_model_filename});
+
+  {
+    bool exists;
+    RETURN_IF_ERROR(FileExists(model_path, &exists));
+    RETURN_ERROR_IF_FALSE(
+        exists, TRITONSERVER_ERROR_UNAVAILABLE,
+        std::string("unable to find '") + model_path +
+            "' for model instance '" + (*state)->Name() + "'");
+  }
+
+  model_state->CreateEngine((*state)->DeviceId(), model_path);
+
+  return nullptr;  // success
+}
+
+ModelInstanceState::ModelInstanceState(
+    ModelState* model_state, TRITONBACKEND_ModelInstance* triton_model_instance)
+    : BackendModelInstance(model_state, triton_model_instance),
+      model_state_(model_state)
+{
+  if (Kind() != TRITONSERVER_INSTANCEGROUPKIND_GPU) {
+    throw triton::backend::BackendModelInstanceException(TRITONSERVER_ErrorNew(
+        TRITONSERVER_ERROR_INVALID_ARG,
+        (std::string("unable to load model '") + model_state_->Name() +
+         "', TensorRT backend supports only GPU device")
+            .c_str()));
+  }
+}
+
+void
+ModelInstanceState::ProcessRequests(
+    TRITONBACKEND_Request** requests, const uint32_t request_count)
+{
+}
+
+/////////////
+
+extern "C" {
+
+// Implementing TRITONBACKEND_Initialize is optional. The backend
+// should initialize any global state that is intended to be shared
+// across all models and model instances that use the backend. But here
+// it simply verify the backend API version is compatible
+TRITONSERVER_Error*
+TRITONBACKEND_Initialize(TRITONBACKEND_Backend* backend)
+{
+  const char* cname;
+  RETURN_IF_ERROR(TRITONBACKEND_BackendName(backend, &cname));
+  std::string name(cname);
+
+  LOG_MESSAGE(
+      TRITONSERVER_LOG_INFO,
+      (std::string("TRITONBACKEND_Initialize: ") + name).c_str());
+
+  // We should check the backend API version that Triton supports
+  // vs. what this backend was compiled against.
+  uint32_t api_version_major, api_version_minor;
+  RETURN_IF_ERROR(
+      TRITONBACKEND_ApiVersion(&api_version_major, &api_version_minor));
+
+  LOG_MESSAGE(
+      TRITONSERVER_LOG_INFO,
+      (std::string("Triton TRITONBACKEND API version: ") +
+       std::to_string(api_version_major) + "." +
+       std::to_string(api_version_minor))
+          .c_str());
+  LOG_MESSAGE(
+      TRITONSERVER_LOG_INFO,
+      (std::string("'") + name + "' TRITONBACKEND API version: " +
+       std::to_string(TRITONBACKEND_API_VERSION_MAJOR) + "." +
+       std::to_string(TRITONBACKEND_API_VERSION_MINOR))
+          .c_str());
+
+  if ((api_version_major != TRITONBACKEND_API_VERSION_MAJOR) ||
+      (api_version_minor < TRITONBACKEND_API_VERSION_MINOR)) {
+    return TRITONSERVER_ErrorNew(
+        TRITONSERVER_ERROR_UNSUPPORTED,
+        "triton backend API version does not support this backend");
+  }
+
+  return nullptr;  // success
+}
+
+// Implementing TRITONBACKEND_Finalize is optional unless state is set
+// using TRITONBACKEND_BackendSetState. The backend must free this
+// state and perform any other global cleanup.
+TRITONSERVER_Error*
+TRITONBACKEND_Finalize(TRITONBACKEND_Backend* backend)
+{
+  // void* vstate;
+  // RETURN_IF_ERROR(TRITONBACKEND_BackendState(backend, &vstate));
+  // auto config = reinterpret_cast<BackendConfiguration*>(vstate);
+  // delete config;
+  return nullptr;  // success
+}
+
+// Implementing TRITONBACKEND_ModelInitialize is optional. The backend
+// should initialize any state that is intended to be shared across
+// all instances of the model.
+TRITONSERVER_Error*
+TRITONBACKEND_ModelInitialize(TRITONBACKEND_Model* model)
+{
+  const char* cname;
+  RETURN_IF_ERROR(TRITONBACKEND_ModelName(model, &cname));
+  std::string name(cname);
+
+  uint64_t version;
+  RETURN_IF_ERROR(TRITONBACKEND_ModelVersion(model, &version));
+
+  LOG_MESSAGE(
+      TRITONSERVER_LOG_INFO,
+      (std::string("TRITONBACKEND_ModelInitialize: ") + name + " (version " +
+       std::to_string(version) + ")")
+          .c_str());
+
+  // With each model we create a ModelState object and associate it
+  // with the TRITONBACKEND_Model.
+  ModelState* model_state = nullptr;
+  RETURN_IF_ERROR(ModelState::Create(model, &model_state));
+  RETURN_IF_ERROR(
+      TRITONBACKEND_ModelSetState(model, reinterpret_cast<void*>(model_state)));
+
+  return nullptr;  // success
+}
+
+// Implementing TRITONBACKEND_ModelFinalize is optional unless state
+// is set using TRITONBACKEND_ModelSetState. The backend must free
+// this state and perform any other cleanup.
+TRITONSERVER_Error*
+TRITONBACKEND_ModelFinalize(TRITONBACKEND_Model* model)
+{
+  void* vstate;
+  RETURN_IF_ERROR(TRITONBACKEND_ModelState(model, &vstate));
+  ModelState* model_state = reinterpret_cast<ModelState*>(vstate);
+
+  LOG_MESSAGE(
+      TRITONSERVER_LOG_INFO, "TRITONBACKEND_ModelFinalize: delete model state");
+
+  delete model_state;
+
+  return nullptr;  // success
+}
+
+// Implementing TRITONBACKEND_ModelInstanceInitialize is optional. The
+// backend should initialize any state that is required for a model
+// instance.
+TRITONSERVER_Error*
+TRITONBACKEND_ModelInstanceInitialize(TRITONBACKEND_ModelInstance* instance)
+{
+  const char* cname;
+  RETURN_IF_ERROR(TRITONBACKEND_ModelInstanceName(instance, &cname));
+  std::string name(cname);
+
+  int32_t device_id;
+  RETURN_IF_ERROR(TRITONBACKEND_ModelInstanceDeviceId(instance, &device_id));
+  TRITONSERVER_InstanceGroupKind kind;
+  RETURN_IF_ERROR(TRITONBACKEND_ModelInstanceKind(instance, &kind));
+
+  LOG_MESSAGE(
+      TRITONSERVER_LOG_INFO,
+      (std::string("TRITONBACKEND_ModelInstanceInitialize: ") + name + " (" +
+       TRITONSERVER_InstanceGroupKindString(kind) + " device " +
+       std::to_string(device_id) + ")")
+          .c_str());
+
+  // The instance can access the corresponding model as well... here
+  // we get the model and from that get the model's state.
+  TRITONBACKEND_Model* model;
+  RETURN_IF_ERROR(TRITONBACKEND_ModelInstanceModel(instance, &model));
+
+  void* vmodelstate;
+  RETURN_IF_ERROR(TRITONBACKEND_ModelState(model, &vmodelstate));
+  ModelState* model_state = reinterpret_cast<ModelState*>(vmodelstate);
+
+  // With each instance we create a ModelInstanceState object and
+  // associate it with the TRITONBACKEND_ModelInstance.
+  ModelInstanceState* instance_state;
+  RETURN_IF_ERROR(
+      ModelInstanceState::Create(model_state, instance, &instance_state));
+  RETURN_IF_ERROR(TRITONBACKEND_ModelInstanceSetState(
+      instance, reinterpret_cast<void*>(instance_state)));
+
+  return nullptr;  // success
+}
+
+// Implementing TRITONBACKEND_ModelInstanceFinalize is optional unless
+// state is set using TRITONBACKEND_ModelInstanceSetState. The backend
+// must free this state and perform any other cleanup.
+TRITONSERVER_Error*
+TRITONBACKEND_ModelInstanceFinalize(TRITONBACKEND_ModelInstance* instance)
+{
+  void* vstate;
+  RETURN_IF_ERROR(TRITONBACKEND_ModelInstanceState(instance, &vstate));
+  ModelInstanceState* instance_state =
+      reinterpret_cast<ModelInstanceState*>(vstate);
+
+  LOG_MESSAGE(
+      TRITONSERVER_LOG_INFO,
+      "TRITONBACKEND_ModelInstanceFinalize: delete instance state");
+
+  delete instance_state;
+
+  return nullptr;  // success
+}
+
+// Implementing TRITONBACKEND_ModelInstanceExecute is required.
+TRITONSERVER_Error*
+TRITONBACKEND_ModelInstanceExecute(
+    TRITONBACKEND_ModelInstance* instance, TRITONBACKEND_Request** requests,
+    const uint32_t request_count)
+{
+  // Triton will not call this function simultaneously for the same
+  // 'instance'. But since this backend could be used by multiple
+  // instances from multiple models the implementation needs to handle
+  // multiple calls to this function at the same time (with different
+  // 'instance' objects). Suggested practice for this is to use only
+  // function-local and model-instance-specific state (obtained from
+  // 'instance'), which is what we do here.
+  ModelInstanceState* instance_state;
+  RETURN_IF_ERROR(TRITONBACKEND_ModelInstanceState(
+      instance, reinterpret_cast<void**>(&instance_state)));
+  ModelState* model_state = instance_state->StateForModel();
+
+  // This backend specifies BLOCKING execution policy. That means that
+  // we should not return from this function until execution is
+  // complete. Triton will automatically release 'instance' on return
+  // from this function so that it is again available to be used for
+  // another call to TRITONBACKEND_ModelInstanceExecute.
+
+  LOG_MESSAGE(
+      TRITONSERVER_LOG_VERBOSE,
+      (std::string("model ") + model_state->Name() + ", instance " +
+       instance_state->Name() + ", executing " + std::to_string(request_count) +
+       " requests")
+          .c_str());
+
+  // At this point we accept ownership of 'requests', which means that
+  // even if something goes wrong we must still return success from
+  // this function. If something does go wrong in processing a
+  // particular request then we send an error response just for the
+  // specific request.
+  // instance_state->ProcessRequests(requests, request_count);
+
+  return nullptr;  // success
+}
+
+}  // extern "C"
+
+}}}  // namespace triton::backend::tensorrt
---
+++ src/tensorrt_utils.h
@@ -0,0 +1,133 @@
+// Copyright (c) 2021, NVIDIA CORPORATION. All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//  * Redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+//  * Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in the
+//    documentation and/or other materials provided with the distribution.
+//  * Neither the name of NVIDIA CORPORATION nor the names of its
+//    contributors may be used to endorse or promote products derived
+//    from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ``AS IS'' AND ANY
+// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+// PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+// OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#pragma once
+
+#include <NvInfer.h>
+#include <string>
+#include <vector>
+
+#include "triton/core/tritonserver.h"
+
+namespace triton { namespace backend { namespace tensorrt {
+
+bool UseTensorRTv2API(const nvinfer1::ICudaEngine* engine);
+
+TRITONSERVER_DataType ConvertTrtTypeToDataType(nvinfer1::DataType trt_type);
+
+std::pair<bool, nvinfer1::DataType> ConvertDataTypeToTrtType(
+    const TRITONSERVER_DataType& dtype);
+
+bool CompareDims(
+    const nvinfer1::Dims& model_dims, const std::vector<int64_t>& dims);
+bool CompareDims(const nvinfer1::Dims& ldims, const nvinfer1::Dims& rdims);
+
+TRITONSERVER_Error* ValidateDimension(
+    const nvinfer1::Dims& this_dims, const nvinfer1::Dims& min_dims,
+    const nvinfer1::Dims& max_dims, const bool skip_first_dimension);
+
+template <typename T>
+TRITONSERVER_Error* ValidateDimension(
+    const T& this_dims, const nvinfer1::Dims& min_dims,
+    const nvinfer1::Dims& max_dims, const bool skip_first_dimension);
+
+TRITONSERVER_Error* CompareDimsSupported(
+    const std::string& model_name, const std::string& tensor_name,
+    const nvinfer1::Dims& model_dims, const std::vector<int64_t>& dims,
+    const bool supports_batching, const bool contains_explicit_batch,
+    const bool compare_exact);
+
+TRITONSERVER_Error* CompareShapeDimsSupported(
+    const std::string& model_name, const std::string& tensor_name,
+    const nvinfer1::Dims& model_dims, const std::vector<int64_t>& dims,
+    const bool supports_batching);
+
+TRITONSERVER_Error* ValidateControlDimsDynamic(
+    const nvinfer1::Dims& dims, const bool support_batching);
+
+TRITONSERVER_Error* ValidateShapeValues(
+    const std::vector<int32_t>& request_shape_values,
+    const int32_t* min_shape_values, const int32_t* max_shape_values,
+    size_t nb_shape_values, const bool support_batching);
+
+TRITONSERVER_Error* MaximumDims(
+    const nvinfer1::Dims& max_profile_dims, const std::vector<int64_t>& dims,
+    const bool support_batching, const int max_batch_size,
+    std::vector<int64_t>* maximum_dims);
+
+void DimsToDimVec(const nvinfer1::Dims& model_dims, std::vector<int64_t>* dims);
+
+bool DimVecToDims(const std::vector<int64_t>& dim_vec, nvinfer1::Dims* dims);
+
+int64_t GetElementCount(const nvinfer1::Dims& dims);
+
+bool ContainsWildcard(const nvinfer1::Dims& dims);
+
+bool ContainsWildcardAtExplicitBatchDim(const nvinfer1::Dims& dims);
+
+const std::string DimsDebugString(const nvinfer1::Dims& dims);
+
+//
+// Templates
+//
+
+template <typename T>
+TRITONSERVER_Error*
+ValidateDimension(
+    const T& this_dims, const nvinfer1::Dims& min_dims,
+    const nvinfer1::Dims& max_dims, const bool skip_first_dimension)
+{
+  const int nonbatch_start_idx = (skip_first_dimension ? 1 : 0);
+  if (int(this_dims.size() + nonbatch_start_idx) != max_dims.nbDims) {
+    return TRITONSERVER_ErrorNew(
+        TRITONSERVER_ERROR_INVALID_ARG,
+        (std::string("model expected ") +
+         std::to_string(max_dims.nbDims - nonbatch_start_idx) +
+         " dimensions but received " + std::to_string(this_dims.size()) +
+         " dimensions")
+            .c_str());
+  }
+
+  for (int i = 0; i < int(this_dims.size()); i++) {
+    if (this_dims[i] == -1) {
+      continue;
+    }
+    if (this_dims[i] < min_dims.d[i + nonbatch_start_idx] ||
+        this_dims[i] > max_dims.d[i + nonbatch_start_idx]) {
+      return TRITONSERVER_ErrorNew(
+          TRITONSERVER_ERROR_INVALID_ARG,
+          (std::string("model expected the shape of dimension ") +
+           std::to_string(i) + " to be between " +
+           std::to_string(min_dims.d[i + nonbatch_start_idx]) + " and " +
+           std::to_string(max_dims.d[i + nonbatch_start_idx]) +
+           " but received " + std::to_string(this_dims[i]))
+              .c_str());
+    }
+  }
+  return nullptr;
+}
+
+}}}  // namespace triton::backend::tensorrt
