1121013a966d55ee04e8bf14599ca1839e171135
krishung5 <krish@nvidia.com>
2022-09-09 11:45:03 -0700

    ifdef out kUINT8 based on TRT version

--- src/tensorrt_utils.cc
+++ src/tensorrt_utils.cc
@@ -25,10 +25,15 @@
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #include "tensorrt_utils.h"
-
+#include <NvInferPlugin.h>
 #include "triton/backend/backend_common.h"
 
 namespace triton { namespace backend { namespace tensorrt {
+
+// Cannot directly use 'NV_TENSORRT_VERSION' in preprocessor because of the
+// syntax.
+int32_t version_ = NV_TENSORRT_VERSION;
+#define TENSORRT_VERSION version
 
 TRITONSERVER_DataType
 ConvertTrtTypeToDataType(nvinfer1::DataType trt_type)
@@ -40,8 +45,10 @@ ConvertTrtTypeToDataType(nvinfer1::DataType trt_type)
       return TRITONSERVER_TYPE_FP16;
     case nvinfer1::DataType::kINT8:
       return TRITONSERVER_TYPE_INT8;
+#if (TENSORRT_VERSION >= 8500)
     case nvinfer1::DataType::kUINT8:
       return TRITONSERVER_TYPE_UINT8;
+#endif  // TENSORRT_VERSION >= 8500
     case nvinfer1::DataType::kINT32:
       return TRITONSERVER_TYPE_INT32;
     case nvinfer1::DataType::kBOOL:
@@ -61,8 +68,10 @@ ConvertTrtTypeToConfigDataType(nvinfer1::DataType trt_type)
       return "TYPE_FP16";
     case nvinfer1::DataType::kINT8:
       return "TYPE_INT8";
+#if (TENSORRT_VERSION >= 8500)
     case nvinfer1::DataType::kUINT8:
       return "TYPE_UINT8";
+#endif  // TENSORRT_VERSION >= 8500
     case nvinfer1::DataType::kINT32:
       return "TYPE_INT32";
     case nvinfer1::DataType::kBOOL:
@@ -117,9 +126,11 @@ ConvertDataTypeToTrtType(const TRITONSERVER_DataType& dtype)
     case TRITONSERVER_TYPE_INT8:
       trt_type = nvinfer1::DataType::kINT8;
       break;
+#if (TENSORRT_VERSION >= 8500)
     case TRITONSERVER_TYPE_UINT8:
       trt_type = nvinfer1::DataType::kUINT8;
       break;
+#endif  // TENSORRT_VERSION >= 8500
     case TRITONSERVER_TYPE_INT32:
       trt_type = nvinfer1::DataType::kINT32;
       break;
