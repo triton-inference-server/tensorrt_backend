ca5d0693f2e11e7fa2cc41b325aaecd618499ebf
Sai Kiran Polisetty <spolisetty@nvidia.com>
2026-01-07 11:23:12 +0530

    fix: Optimize TensorRT backend to avoid duplicate host-memory allocation during engine deserialization (#118)

--- src/loader.cc
+++ src/loader.cc
@@ -1,4 +1,4 @@
-// Copyright 2021-2023, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+// Copyright 2021-2026, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions
@@ -79,10 +79,9 @@ LoadPlan(
 
   std::string model_data_str;
   RETURN_IF_ERROR(ReadTextFile(plan_path, &model_data_str));
-  std::vector<char> model_data(model_data_str.begin(), model_data_str.end());
 
-  engine->reset(
-      (*runtime)->deserializeCudaEngine(&model_data[0], model_data.size()));
+  engine->reset((*runtime)->deserializeCudaEngine(
+      model_data_str.data(), model_data_str.size()));
   if (*engine == nullptr) {
     return TRITONSERVER_ErrorNew(
         TRITONSERVER_ERROR_INTERNAL,
